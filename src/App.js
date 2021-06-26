1c4e4b1dfcf3747b69245f85fe0c29744606c5a0
Sakshi <sakshi.kapoor@box8.in>
2021-06-26 17:17:16 +0530

    added save option and heading option

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Step from './Step';
 import './App.css';
 import refresh from './images/refresh.svg';
+import printer from './images/printer.png';
 
 function refreshPage() {
   window.location.reload();
+}
+
+function print() {
+  var printContents = document.getElementById('flow').innerHTML;
+  var originalContents = document.body.innerHTML;
+
+  document.body.innerHTML = printContents;
+
+  window.print();
+
+  document.body.innerHTML = originalContents;
 }
 
 function App() {
@@ -15,11 +27,14 @@ function App() {
   return (
     <div className="background">
       <div className="heading">Tree FlowChart Maker</div>
-      <h2>Click on element to add text</h2>
-      <div className="flow" id="flow">
-        <Step details={initialDetails} />
+      <div id="flow">
+        <input className="flowchart-name" type="text" placeholder="Click on element to add text" />
+        <div className="flow">
+          <Step details={initialDetails} />
+        </div>
       </div>
       <img src={refresh} className="refresh" alt="refresh" onClick={refreshPage} />
+      <img src={printer} className="print" alt="save" onClick={print} />
     </div>
   );
 }

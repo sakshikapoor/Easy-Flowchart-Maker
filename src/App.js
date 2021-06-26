9c1c5177ada277ff03d60b28155873e5910cf5a1
Sakshi <sakshi.kapoor@box8.in>
2021-06-26 18:24:48 +0530

    bugfixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Step from './Step';
 import './App.css';
+import { useState } from "react";
 import refresh from './images/refresh.svg';
 import printer from './images/printer.png';
 
@@ -7,34 +8,35 @@ function refreshPage() {
   window.location.reload();
 }
 
-function print() {
-  var printContents = document.getElementById('flow').innerHTML;
-  var originalContents = document.body.innerHTML;
-
-  document.body.innerHTML = printContents;
-
-  window.print();
-
-  document.body.innerHTML = originalContents;
-}
-
 function App() {
+  const [display, setDisplay] = useState(true);
   const initialDetails = {
     type: 'start',
     children: [],
     level: 0
   }
+
+  const print = () => {
+    setDisplay(false);
+    setTimeout(() => {
+      window.print();
+      setDisplay(true);
+    }, 0)
+  }
+
   return (
     <div className="background">
-      <div className="heading">Tree FlowChart Maker</div>
+      <div className={`${display ? "show" : "hide"}`}>
+        <div className="heading">Tree FlowChart Maker</div>
+        <img src={refresh} className="refresh" alt="refresh" onClick={refreshPage} />
+        <img src={printer} className="print" alt="save" onClick={print} />
+      </div>
       <div id="flow">
         <input className="flowchart-name" type="text" placeholder="Click on element to add text" />
         <div className="flow">
           <Step details={initialDetails} />
         </div>
       </div>
-      <img src={refresh} className="refresh" alt="refresh" onClick={refreshPage} />
-      <img src={printer} className="print" alt="save" onClick={print} />
     </div>
   );
 }
